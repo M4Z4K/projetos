7f5d7d472822dfea9358b97db12093c704731fee
M4Z4K <125928761+M4Z4K@users.noreply.github.com>
2026-04-14 10:57:27 -0300

    fix: correção do range incorreto do ciclo de repetição, alteração nos inputs e impressões para melhorar a interface (no terminal). feat:  inclusão da apresentação da porcentagem com duas casas decimais (double float).

--- pesquisa-opiniao/pesquisa-opiniao.py
+++ pesquisa-opiniao/pesquisa-opiniao.py
@@ -5,41 +5,41 @@ def main():
     bom = 0
     ruim = 0
 
-    for entrevistados in range(1,50):
-        nome = input("Digite o seu nome: ")
+    for entrevistados in range(1,51): # Correção do range que estava incorreto, coletando apenas 49 respostas, e não 50, conforme o enunciado.
+        nome = input("Digite o seu nome: \n")
         while True:
             try:
-                idade = int(input("Digite sua idade: "))
+                idade = int(input("Digite sua idade: \n"))
                 if idade >= 0:
                     break
                 else:
-                    print("Por favor, insira uma idade válida para prosseguirmos")
+                    print("Por favor, insira uma idade válida para prosseguirmos\n")
             except ValueError:
-                print("Por favor, insira uma idade válida para prosseguirmos")
+                print("Por favor, insira uma idade válida para prosseguirmos\n")
         
         while True:
             avaliacao = input("Avalie o atendimento prestado conforme as indicações abaixo:\nDigite 1 para EXCELENTE\nDigite 2 para BOM\nDigite 3 para RUIM\n")
             if avaliacao in ["1", "2", "3"]:
                 match avaliacao:
                     case "1":
-                        print("Agradecemos sua participação!")
+                        print("Agradecemos sua participação!\n")
                         excelente += 1
                         break
                     case "2":
-                        print("Agradecemos sua participação!")
+                        print("Agradecemos sua participação!\n")
                         bom += 1
                         break
                     case "3":
-                        print("Agradecemos sua participação!")
+                        print("Agradecemos sua participação!\n")
                         ruim += 1
                         break
-            else: print("Entrada inválida, por favor digite o número correspondente a sua avaliação.")
+            else: print("Entrada inválida, por favor digite o número correspondente a sua avaliação.\nAvalie o atendimento prestado conforme as indicações abaixo:\nDigite 1 para EXCELENTE\nDigite 2 para BOM\nDigite 3 para RUIM\n")
 
     excPorcentagem = excelente * 100 / entrevistados
     bomPorcentagem = bom * 100 / entrevistados
     ruimPorcentagem = ruim * 100 / entrevistados
 
-    print("Pesquisa finalizada!\nAvaliações Excelentes:",excelente,"(",excPorcentagem,"% )\nAvaliações Ruins:",ruim,"(",ruimPorcentagem,"% )\n")
+    print("Pesquisa finalizada!\nAvaliações Excelentes:",excelente, f"({excPorcentagem:.2f}%)\nAvaliações Ruins:",ruim,f"({excPorcentagem:.2f}%)\n") # Alteração da forma de apresentação da porcentagem (inserido double float).
 
 if __name__ == "__main__":
     main()
